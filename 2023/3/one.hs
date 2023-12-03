4b7fa8d536968e7869dbb0954c5358766c67a849
Alan Davidson <alan@viam.com>
2023-12-03 01:35:19 -0500

    Day 3 part 1 (#2)
    I'm going to bed, might do part 2 tomorrow.
    
    People working in languages where they get pointers and random access to the middle of arrays using indices will have a much easier time with this!
    
    
    * tweak to day 2: alphabetize imports
    
    * input for day 3
    
    * start of skeleton
    
    * code feels like it ought to work, but doesn't. Needs debugging
    
    * split out subpieces for debugging
    
    * add test schematic for simpler debugging
    
    * bugfixes: part 1 works
    
    * simplify
    
    * try to remove some duplication
    
    * even simpler
    
    * further tweak
    
    * more obvious variable names

---
+++ 2023/3/one.hs
@@ -0,0 +1,56 @@
+import Data.Char(digitToInt, isDigit)
+
+
+isSymbol :: Char -> Bool
+isSymbol = and . (map (/=) "1234567890." <*>) . pure
+
+
+-- In the output, the first bool is whether this digit is adjacent to a symbol,
+-- and the second one is whether it is immediately followed by another digit as
+-- part of the same number.
+findDigits :: [String] -> [(Int, Bool, Bool)]
+findDigits schematic = concat $ zipWith3 findDigitsInRow (init $ init schematic)
+                                                         (tail $ init schematic)
+                                                         (tail $ tail schematic)
+
+
+findDigitsInRow :: String -> String -> String -> [(Int, Bool, Bool)]
+findDigitsInRow above target below = let
+    checkColumn above target below =
+        (if isDigit target then Just (digitToInt target) else Nothing,
+         any isSymbol [above, target, below])
+    columnData = zipWith3 checkColumn above target below
+    checkNeighbors (aVal, aSym) (bVal, bSym) (cVal, cSym) =
+        (bVal, or [aSym, bSym, cSym], cVal /= Nothing)
+    neighborData = zipWith3 checkNeighbors (init $ init columnData)
+                                           (init $ tail columnData)
+                                           (tail $ tail columnData)
+  in
+    -- There must be a better way to do this, but I couldn't think of it...
+    map (\(Just x, y, z) -> (x, y, z)) .
+        filter (\(x, _, _) -> x /= Nothing) $ neighborData
+
+
+-- Surely this is just a super-complicated fold, but I couldn't see it.
+findPartNumbers :: [(Int, Bool, Bool)] -> [Int]
+findPartNumbers = findPartNumbers' 0 False
+  where
+    findPartNumbers' _ _ [] = []
+    findPartNumbers' total bySymbol ((value, bySymbol', continued):rest) = let
+        newTotal = 10 * total + value
+        nearSymbol = bySymbol || bySymbol'
+      in
+        if continued
+        then findPartNumbers' newTotal nearSymbol rest
+        else (if nearSymbol then (newTotal :) else id) $ findPartNumbers rest
+
+
+pad :: [String] -> [String]
+pad schematic = let
+    allDots = '.' : allDots
+  in
+    allDots : map (('.' :) . (++ ".")) schematic ++ [allDots]
+
+
+main :: IO()
+main = getContents >>= print . sum . findPartNumbers . findDigits . pad . lines
