361d9189a1936c89d48627b764f518922017c1cf
Alan Davidson <alan@viam.com>
2023-12-04 22:58:49 -0500

    Day 4 (#4)
    To get `HashSet`, I broke down and set up Cabal. It puts the binary in some ridiculously deep location; I wonder if I can simplify that.
    
    
    * add inputs
    
    * I think I've written good code, but need to install Data.HashSet before debugging
    
    * split up long line, still can't compile without stack
    
    * forgot an import, still haven't compiled
    
    * get the types right, even if you can't run the type checker yet
    
    * numbers should have more than 0 characters in them
    
    * finish part 1
    
    * rename one.hs -> combined.hs for part 2
    
    * debugging
    
    * cleanup for right answer
    
    * tweaks
    
    * more tweaks
    
    * comment about a gotcha I didn't figure out

---
+++ 2023/4/combined.hs
@@ -0,0 +1,55 @@
+import Control.FromSum(fromEither)
+import Data.HashSet(HashSet, fromList, intersection, size)
+import Text.Parsec(endBy, eof, many1, parse, sepBy)
+import Text.Parsec.Char(char, digit, string)
+
+
+data Scratch = Scratch{ticketId :: Int, winners :: [Int], nums :: [Int]}
+
+
+parseGame :: String -> Scratch
+parseGame = fromEither (error . show) . parse parseLine ""
+  where
+    parseLine = do
+        string "Card" >> whitespace
+        ticketId <- parseNumber
+        char ':' >> whitespace
+        -- Sneaky trouble here: intuitively it's `sepBy parseNumber whitespace` followed by
+        -- `whitespace >> char '|' >> whitespace`. but the sepBy tries matching the whitespace for
+        -- another number, and then the unexpected '|' pushes it to go on. There ought to be a way
+        -- to avoid this with `Parsec.try`, but I haven't figured it out yet.
+        winners <- endBy parseNumber whitespace
+        char '|' >> whitespace
+        nums <- sepBy parseNumber whitespace
+        eof
+        return Scratch{ticketId=ticketId, winners=winners, nums=nums}
+    whitespace = many1 (char ' ')
+    parseNumber = many1 digit >>= return . read
+
+
+winningNums :: Scratch -> HashSet Int
+winningNums s = intersection (fromList $ winners s) (fromList $ nums s)
+
+
+score :: HashSet Int -> Int
+score x  = if x == mempty then 0 else 2 ^ (size x - 1)
+
+
+addCards :: [Int] -> [Int]
+addCards = addCards' startingCards
+  where
+    startingCards = 1 : startingCards
+    addCards' _              []             = []
+    addCards' (count:counts) (score:scores) = count : addCards' counts' scores
+      where
+        counts' = applyToN score (+ count) counts
+        applyToN 0 _ rest   = rest
+        applyToN n f (x:xs) = f x : applyToN (n - 1) f xs
+
+
+main :: IO()
+main = do
+    stdin <- getContents
+    let games = map parseGame . lines $ stdin
+    print . sum . map (score . winningNums) $ games
+    print . sum . addCards . map (size . winningNums) $ games
