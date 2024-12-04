e9111c3b0bae11fd5c5239a74e725b8a1a0a3070
Alan Davidson <alan@viam.com>
2024-12-04 16:19:11 -0500

    tweaks

--- 2024/4/two.hs
+++ 2024/4/two.hs
@@ -4,7 +4,7 @@ import Data.Maybe(fromMaybe)
 
 searchWords :: [String] -> Int
 searchWords block = let
-    getFrom x y = (block !? y) >>= (!? x)
+    getFrom x y = (block !? x) >>= (!? y)
     equals x y v = fromMaybe False $ getFrom x y >>= (return . (== v))
     forwardDiag x y dx dy = equals (x + dx) (y + dy) 'M' &&
                             equals (x - dx) (y - dy) 'S'
@@ -12,8 +12,8 @@ searchWords block = let
     cross x y = diag x y 1 1 && diag x y 1 (-1)
     findAt x y = equals x y 'A' && cross x y
   in
-    length . filter id $ [findAt x y | x <- [0..(length block - 1)],
-                                       y <- [0..(length (head block) - 1)]]
+    length . filter id $ [findAt x y | x <- [0..(length block - 1)]
+                                     , y <- [0..(length (head block) - 1)]]
 
 
 main :: IO ()
