7a036fe90c2e04ba1b601f10d6ce08acc50b8010
Alan Davidson <alan@viam.com>
2024-12-11 16:05:21 -0500

    day 11 part 2

--- 2024/11/solution.hs
+++ 2024/11/solution.hs
@@ -1,21 +1,24 @@
-import GHC.Utils.Misc(nTimes)
+import qualified Data.MemoCombinators as Memoize
 
 
-blink :: [Int] -> [Int]
-blink [] = []
-blink (0 : rest) = 1 : blink rest
-blink (value : rest) = let
-    str = show value
-    len = length str
-    firstHalf  = read . take (len `div` 2) $ str
-    secondHalf = read . drop (len `div` 2) $ str
-  in
-    if len `mod` 2 == 0
-    then firstHalf : secondHalf : blink rest
-    else value * 2024 : blink rest
+blink :: Int -> Int -> Int
+blink = Memoize.memo2 Memoize.integral Memoize.integral blink'
+  where
+    blink' 0 _     = 1
+    blink' n 0     = blink (n - 1) 1
+    blink' n value = let
+        str = show value
+        len = length str
+        firstHalf  = read . take (len `div` 2) $ str
+        secondHalf = read . drop (len `div` 2) $ str
+      in
+        if len `mod` 2 == 0
+        then blink (n - 1) firstHalf + blink (n - 1) secondHalf
+        else blink (n - 1) (value * 2024)
 
 
 main :: IO ()
 main = do
     contents <- getContents
-    print . length . nTimes 25 blink . map (read) . words $ contents
+    print . sum . map (blink 25 . read) . words $ contents
+    print . sum . map (blink 75 . read) . words $ contents
